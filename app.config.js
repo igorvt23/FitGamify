ad1c36a7fd69c0295cc89619f8372db04b1383f0
Igor Viana Tomaz <igorvianat@gmail.com>
2026-04-08 23:04:36 -0300

    chore(eas): add projectId to dynamic config and set appVersionSource to remote

--- app.config.js
+++ app.config.js
@@ -4,7 +4,11 @@ module.exports = {
   ...expo,
   extra: {
     ...(expo.extra ?? {}),
+    eas: {
+      ...((expo.extra && expo.extra.eas) ?? {}),
+      projectId: "3a7cc4b0-ba85-4c01-9210-764aa44016aa"
+    },
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? "",
-    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "",
-  },
+    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? ""
+  }
 };
